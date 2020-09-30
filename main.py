848c33c52bc04cb6c8c76e5d954c8b9b9dbf19fd
coder <anuingale34@gmail.com>
2020-09-30 13:34:05 +0530

    Update Code

---
+++ main.py
@@ -0,0 +1,32 @@
+import json
+import  requests
+import  datetime
+
+url = 'http://api.openweathermap.org/'
+api_key = '271d1234d3f497eed5b1d80a07b3fcd1'
+complete_url = url +  'data/2.5/weather?q=Yavatmal,india&appid=' + api_key
+response = requests.get(complete_url)
+x = response.json()
+date_name = x['dt']
+date_m = datetime.datetime.fromtimestamp(date_name)
+current_day = date_m.strftime('%d')
+
+num = int(current_day)
+# num = 10
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            # msg ={ num : 'is not a prime number so no Date'}
+            # print(json.dumps(msg))
+            print('Date is not a prime number so no Date')
+            break
+    else:
+        # msg = response.json()
+        # print(json.dumps(msg))
+        print(response.json())
+else:
+    # msg = {num : 'is not a prime number so no Date'}
+    # print(json.dumps(msg))
+    print('Date is not a prime number so no Date')
+
+
